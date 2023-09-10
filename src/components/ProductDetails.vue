1be54b2de93d689d2a9657077bdc523f567cfcd5
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2023-09-11 00:34:55 +0300

    ProductDetails

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h3>{{ product.name }}</h3>
+        <h2>{{ formattedPrice }}</h2>
+        <p class="available" v-if="product.available">Available</p>
+        <p class="out" v-else>Out of stock</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductDetails',
+
+    data() {
+        return {
+            product: {
+                name: 'Валирийский меч',
+                price: 199.99,
+                available: true,
+            }
+        };
+    },
+
+    computed: {
+        formattedPrice() {
+            return `${this.product.price.toFixed(2)} руб.`
+        }
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .available {
+        color: green;
+    }
+
+    .out {
+        color: red;
+    }
+</style>
